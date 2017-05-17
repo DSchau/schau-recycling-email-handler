61c1a195a13db721ed94a46743329c76ef68d405
Dustin Schau <dustinschau@gmail.com>
2017-05-16 19:40:06 -0500

    feat: add an html template

--- src/util/send.ts
+++ src/util/send.ts
@@ -2,6 +2,7 @@ import * as nodeMailer from 'nodemailer';
 import * as config from 'config';
 
 import { toJson } from './to-json';
+import { template as html } from './template';
 import { validator } from './validator';
 
 export function send(body: string): Promise<string> {
@@ -22,10 +23,10 @@ export function send(body: string): Promise<string> {
     });
 
     const options = {
-      from: `"${data.name}" <${data.email}>`,
+      from: `"${data.name}" <${data.email || 'dustinschau@gmail.com'}>`,
       to: 'dustinschau+website@gmail.com',
       subject: `${data.subject || 'Hello from dustinschau.com'}`,
-      text: data.message || ''
+      html: html(data)
     };
 
     transporter.sendMail(options, (error, info) => {
